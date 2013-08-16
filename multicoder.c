6971779f41345bf7fc5345a905b4dbacaf96418e
Andy Armstrong <andy@hexten.net>
2013-08-16 16:53:13 +0100

    Optional decode path WIP.

--- multicoder.c
+++ multicoder.c
@@ -22,6 +22,17 @@ static void *hls_muxer(void *ctx) {
   return NULL;
 }
 
+typedef struct {
+  jd_var *cfg;
+  mc_queue *in, *out;
+  pthread_t t;
+} decoder;
+
+static void h264_decoder(void *ctx) {
+  decoder *c = ctx;
+  mc_h264_decode(c->cfg, c->in, c->out);
+}
+
 static int dts_compare(mc_queue_entry *a, mc_queue_entry *b, void *ctx) {
   (void) ctx;
   return a->d.pkt.dts < b->d.pkt.dts ? -1 : a->d.pkt.dts > b->d.pkt.dts ? 1 : 0;
@@ -29,16 +40,6 @@ static int dts_compare(mc_queue_entry *a, mc_queue_entry *b, void *ctx) {
 
 int main(int argc, char *argv[]) {
   AVFormatContext *fcx = NULL;
-  muxer hls;
-
-  mc_queue *aq = mc_queue_new(100);
-  mc_queue *vq = mc_queue_new(100);
-
-  hls.qm = mc_queue_merger_new(dts_compare, NULL);
-  mc_queue_merger_add(hls.qm, aq);
-  mc_queue_merger_add(hls.qm, vq);
-
-  mc_info("Starting multicoder");
 
   av_register_all();
   avcodec_register_all();
@@ -48,18 +49,63 @@ int main(int argc, char *argv[]) {
   if (avformat_open_input(&fcx, argv[1], NULL, NULL) < 0)
     jd_throw("Can't open %s", argv[1]);
 
-  pthread_create(&hls.t, NULL, hls_muxer, &hls);
+  mc_info("Starting multicoder");
 
-  mc_demux(fcx, aq, vq);
-  mc_queue_packet_put(aq, NULL);
-  mc_queue_packet_put(vq, NULL);
+  if (1) {
+    muxer hls;
 
-  pthread_join(hls.t, NULL);
+    mc_queue *aq = mc_queue_new(100);
+    mc_queue *vq = mc_queue_new(100);
 
-  avformat_close_input(&fcx);
-  mc_queue_merger_free(hls.qm);
-  mc_queue_free(aq);
-  mc_queue_free(vq);
+    hls.qm = mc_queue_merger_new(dts_compare, NULL);
+    mc_queue_merger_add(hls.qm, aq);
+    mc_queue_merger_add(hls.qm, vq);
+
+    pthread_create(&hls.t, NULL, hls_muxer, &hls);
+
+    mc_demux(fcx, aq, vq);
+    mc_queue_packet_put(aq, NULL);
+    mc_queue_packet_put(vq, NULL);
+
+    pthread_join(hls.t, NULL);
+
+    avformat_close_input(&fcx);
+    mc_queue_merger_free(hls.qm);
+    mc_queue_free(aq);
+    mc_queue_free(vq);
+  }
+  else {
+    muxer hls;
+    decoder dec;
+
+    mc_queue *haq = mc_queue_new(100);
+    mc_queue *hvq = mc_queue_new(100);
+    mc_queue *dvq = mc_queue_new(100);
+
+    dec.in = hvq;
+    dec.out = dvq;
+
+    hls.qm = mc_queue_merger_new(dts_compare, NULL);
+    mc_queue_merger_add(hls.qm, haq);
+    mc_queue_merger_add(hls.qm, dvq);
+
+    pthread_create(&hls.t, NULL, hls_muxer, &hls);
+    pthread_create(&dec.t, NULL, h264_decoder, &dec);
+
+    mc_demux(fcx, haq, hvq);
+    mc_queue_packet_put(haq, NULL);
+    mc_queue_packet_put(hvq, NULL);
+
+    pthread_join(hls.t, NULL);
+    pthread_join(dec.t, NULL);
+
+    avformat_close_input(&fcx);
+    mc_queue_merger_free(hls.qm);
+
+    mc_queue_free(haq);
+    mc_queue_free(hvq);
+    mc_queue_free(dvq);
+  }
 
   return 0;
 }
