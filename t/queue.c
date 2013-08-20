5995815f25b8832f69b24876b731adc0e33cc1a3
Andy Armstrong <andy@hexten.net>
2013-08-20 16:05:58 +0100

    Test coverage for mc_queue_multi_packet_put.

--- t/queue.c
+++ t/queue.c
@@ -10,6 +10,16 @@
 
 #include "mc_queue.h"
 
+static int drain(mc_queue *q) {
+  AVPacket pkt;
+  int count = 0;
+  while (mc_queue_peek(q)) {
+    mc_queue_packet_get(q, &pkt);
+    count++;
+  }
+  return count;
+}
+
 static void test_non_full(void) {
   AVPacket pkt;
   mc_queue *q = mc_queue_new(10);
@@ -27,11 +37,7 @@ static void test_non_full(void) {
     if (!ok(q->used == 5, "try %d: q->used == 5 (during)", t))
       diag("q->used = %d", q->used);
 
-    int count = 0;
-    while (mc_queue_peek(q)) {
-      mc_queue_packet_get(q, &pkt);
-      count++;
-    }
+    int count = drain(q);
 
     if (!ok(count == 5, "try %d: five packets queued, retrieved", t))
       diag("count = %d", count);
@@ -43,9 +49,32 @@ static void test_non_full(void) {
   mc_queue_free(q);
 }
 
+static void test_multi(void) {
+  AVPacket pkt;
+  mc_queue *head = mc_queue_new(0);
+  mc_queue *q1 = mc_queue_new(10);
+  mc_queue *q2 = mc_queue_new(10);
+
+  mc_queue_hook(head, q1);
+  mc_queue_hook(head, q2);
+
+  for (unsigned i = 0; i < 5; i++)
+    mc_queue_multi_packet_put(head, &pkt);
+
+  ok(!mc_queue_peek(head), "nothing in head");
+
+  ok(drain(q1) == 5, "5 in q1");
+  ok(drain(q2) == 5, "5 in q2");
+
+  mc_queue_free(head);
+  mc_queue_free(q1);
+  mc_queue_free(q2);
+}
+
 void test_main(void) {
   scope {
     test_non_full();
+    test_multi();
   }
 }
 
